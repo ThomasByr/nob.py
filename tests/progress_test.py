e01366d2462f4d425fdea77f25b831c1367e90d6
Thomas Byr <thomas.bouyer@pm.me>
2026-05-10 23:32:57 +0200

    test: add progress bar tests

---
+++ tests/progress_test.py
@@ -0,0 +1,67 @@
+from pytest import CaptureFixture
+
+from nob.progress import progress, track
+
+
+def test_default_description(capsys: CaptureFixture[str]):
+    for _ in track(range(1)):
+        pass
+    captured = capsys.readouterr()
+    assert captured.out.startswith("Working...")
+
+
+def test_set_description(capsys: CaptureFixture[str]):
+    d0 = "Some non-default description"
+    d1 = "Some other description text"
+
+    for _ in track(range(1), d0):
+        pass
+    captured = capsys.readouterr()
+    assert captured.out.startswith(d0)
+
+    bar = progress()
+    task = bar.add_task(d1)
+    with bar:
+        bar.advance(task)
+    captured = capsys.readouterr()
+    assert captured.out.startswith(d1)
+
+
+def test_set_total(capsys: CaptureFixture[str]):
+    for _ in track(range(1), total=123):
+        pass
+    captured = capsys.readouterr()
+    assert "1/123" in captured.out
+
+    bar = progress()
+    task = bar.add_task("", total=7)
+    with bar:
+        bar.advance(task)
+    captured = capsys.readouterr()
+    assert "1/7" in captured.out
+
+
+def test_no_known_total(capsys: CaptureFixture[str]):
+    for _ in track(range(42), total=None):
+        pass
+    captured = capsys.readouterr()
+    assert "42" in captured.out and "42/" not in captured.out
+
+    bar = progress(known_total=False)
+    task = bar.add_task("", total=None)
+    with bar:
+        bar.advance(task, 700)
+    captured = capsys.readouterr()
+    assert "700" in captured.out and "700/" not in captured.out
+
+
+def test_auto_total(capsys: CaptureFixture[str]):
+    for _ in track(range(19)):
+        pass
+    captured = capsys.readouterr()
+    assert "19/19" in captured.out
+
+    for _ in track(range(35)):
+        pass
+    captured = capsys.readouterr()
+    assert "35/35" in captured.out
