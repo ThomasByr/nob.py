67a102d53b2e022b95812dfecc740ee8e170a1e2
Thomas Byr <thomas.bouyer@pm.me>
2026-05-25 14:26:00 +0200

    chore: moved logging handlers into __init__

--- src/nob/logging/__init__.py
+++ src/nob/logging/__init__.py
@@ -1,1 +1,40 @@
-from .handler import *
+import logging
+from logging.handlers import RotatingFileHandler
+
+from rich.logging import RichHandler
+
+__all__ = ["init_handler", "mute_logger"]
+
+
+def mute_logger(*names: str) -> None:
+    """Mute the specified logger by setting its level to WARNING.
+
+    Args:
+        name (str): The name of the logger to mute.
+    """
+    for name in names:
+        logger = logging.getLogger(name)
+        logger.setLevel(logging.WARNING)
+        logger.propagate = False  # prevent logs from propagating to root logger
+
+
+def init_handler(log_level: int = logging.INFO, log_file: str | None = None):
+    """Initialize logging handlers for the application.
+
+    Args:
+        log_level (int, optional): The logging level to set. Defaults to logging.INFO.
+        log_file (str, optional): The path where logs will be written. If None, logs will not be written to a file.
+    """
+    handlers: list = [RichHandler()]  # modifies the record first for RotatingFileHandler
+    if log_file:
+        handlers.append(RotatingFileHandler(log_file, maxBytes=1024 * 1024 * 100, backupCount=5))
+    logging.basicConfig(
+        level=log_level,
+        format="%(message)s",
+        datefmt="[%X]",
+        handlers=handlers,
+        force=True,
+    )
+
+    # mute overly verbose loggers
+    mute_logger("urllib3", "rich")
