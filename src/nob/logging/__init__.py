72b13692ab323ab403454fea960c16e12b33867c
Thomas Byr <thomas.bouyer@pm.me>
2026-06-05 19:37:31 +0200

    feat: support rotating file handler arguments in init in logging module

--- src/nob/logging/__init__.py
+++ src/nob/logging/__init__.py
@@ -18,16 +18,25 @@ def mute_logger(*names: str) -> None:
         logger.propagate = False  # prevent logs from propagating to root logger
 
 
-def init_handler(log_level: int = logging.INFO, log_file: str | None = None):
+def init_handler(
+    log_level: int = logging.INFO,
+    log_file: str | None = None,
+    max_bytes: int | None = None,
+    backup_count: int | None = None,
+) -> None:
     """Initialize logging handlers for the application.
 
     Args:
         log_level (int, optional): The logging level to set. Defaults to logging.INFO.
         log_file (str, optional): The path where logs will be written (using `RotatingFileHandler`). If None, logs will not be written to a file.
+        max_bytes (int, optional): The maximum size of the log file before it is rotated. Defaults to 10 MB per file.
+        backup_count (int, optional): The number of backup log files to keep. Defaults to 5.
     """
     handlers: list = [RichHandler()]  # modifies the record first for RotatingFileHandler
+    max_bytes = max_bytes if max_bytes is not None else 10 * 1024 * 1024
+    backup_count = backup_count if backup_count is not None else 5
     if log_file:
-        handlers.append(RotatingFileHandler(log_file, maxBytes=1024 * 1024 * 100, backupCount=5))
+        handlers.append(RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count))
     logging.basicConfig(
         level=log_level,
         format="%(message)s",
